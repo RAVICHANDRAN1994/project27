c382f0a014b9dad7b1772261006e88c4d4ca9d20
RAVICHANDRAN1994 <ravichandran94.udt@gmail.com>
2023-03-27 12:20:47 +0530

    added

---
+++ src/java.java
@@ -0,0 +1,11 @@
+
+public class java {
+public static void main(String[] args) {
+	String[] arr = { "abc", "xyz", "abc"," pqr"};
+	int count = 0;
+for (int i = 0; i < arr.length; i++) {
+System.out.println(count);
+}
+	
+}
+}
